2da59ad4c2dca2912d73ee3f6ef0be461a5818db
Lili <iliyanastefanova@msn.com>
2021-01-23 09:01:38 -0500

    button

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'iliyanastef@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -21,7 +21,7 @@
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  $contact->subject = 'New Booking Request';
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
@@ -33,9 +33,17 @@
   );
   */
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['firstname'], 'First name');
+  $contact->add_message( $_POST['lastname'], 'Last name');
+  $contact->add_message( $_POST['telephone'], 'Tel');
+  $contact->add_message( $_POST['email'], 'Email');  
+  $contact->add_message( $_POST['address'], 'Address');
+  $contact->add_message( $_POST['type'], 'Type of vehicle');
+  $contact->add_message( $_POST['time'], 'Requested time');
+  $contact->add_message( $_POST['servicetype'], 'Service package');
+  $contact->add_message( $_POST['addon'], 'addon');
   $contact->add_message( $_POST['message'], 'Message', 10);
+
 
   echo $contact->send();
 ?>
