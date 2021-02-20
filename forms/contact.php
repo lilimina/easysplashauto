dc1d32c7d94bcf543833e0896f36f22ba2ee7f4f
Lili <iliyanastefanova@msn.com>
2021-02-20 15:49:32 -0500

    final touches

--- forms/contact.php
+++ forms/contact.php
@@ -1,7 +1,7 @@
 <?php
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'iliyanastef@gmail.com';
+  $receiving_email_address = 'contact@eazyautosplash.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -37,7 +37,6 @@
   $contact->add_message( $selectOption = $_POST['servicetype'], 'Service package');
   $contact->add_message( $selectOption = $_POST['addon'], 'Add on');
   $contact->add_message( $_POST['message'], 'Message', 10);
-
 
   echo $contact->send();
 ?>
