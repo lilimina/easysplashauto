494f7d4fe20ccb63be1ef8d1fa096664fdb08fce
Lili <iliyanastefanova@msn.com>
2021-02-20 13:36:39 -0500

    mailchimp form

--- forms/contact.php
+++ forms/contact.php
@@ -19,7 +19,7 @@
   $contact->ajax = true;
   
   $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
+  $contact->from_name = $_POST['firstname'];
   $contact->from_email = $_POST['email'];
   $contact->subject = 'New Booking Request';
 
